8c4d948ff837b9c39c92d8a68d440712975d80f0
SanthoshKumar <santhoshkumar9028@gmail.com>
2024-06-16 17:09:19 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Alert, Button, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Button>click</Button>
+      <Alert variant="filled">
+        <Typography component="h1">Santhoshkumar</Typography>
+      </Alert>
+    </>
+  );
+}
+
+export default App;
