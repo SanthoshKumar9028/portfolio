07c5bfea7804d121c1e8ad041b474a4594672e44
Santhosh <71515865+SanthoshKumar9028@users.noreply.github.com>
2024-12-26 07:44:09 +0000

    Fixed lint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Banner from "@/components/Banner";
-import NavBar from "@/components/NavBar";
 
 function App() {
   return <Banner />;
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,4 @@
-import { useState } from "react";
-
 function NavBar() {
-  const [open, setOpen] = useState(false);
-
-  const toggleDrawer = (openState: boolean) => () => {
-    setOpen(openState);
-  };
-
   return "NavBar";
 }
 
